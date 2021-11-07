5363cdf87ef8b605b1416c9035f9e4fe7a37f2d5
jwl-97 <jwlee_97@naver.com>
2021-11-07 14:00:30 +0900

    add onboarding

--- week 1-Jetpack Compose basics/week1/app/src/main/java/com/ljw/week1/MainActivity.kt
+++ week 1-Jetpack Compose basics/week1/app/src/main/java/com/ljw/week1/MainActivity.kt
@@ -3,64 +3,91 @@ package com.ljw.week1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ljw.week1.ui.theme.Week1Theme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
             Week1Theme {
-                myApp(listOf("World", "Compose"))
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun myApp(names: List<String>) {
-    Column(modifier = Modifier.padding(vertical = 4.dp)) {
-        for (name in names) {
-            Greeting(name)
+fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
+    } else {
+        Greetings()
+    }
+}
+
+@Composable
+fun OnboardingScreen(onContinueClicked: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
     val extraPadding = if (expanded.value) 48.dp else 0.dp
 
     Surface(
         color = MaterialTheme.colors.primary,
-        modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp)
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier
-                    .weight(1F)
+                    .weight(1f)
                     .padding(bottom = extraPadding)
             ) {
-                Text(text = "Hello,")
-                Text(text = "$name!")
+                Text(text = "Hello, ")
+                Text(text = name)
             }
 
-            OutlinedButton(onClick = { expanded.value = !expanded.value }) {
+            OutlinedButton(
+                onClick = { expanded.value = !expanded.value }
+            ) {
                 Text(if (expanded.value) "Show less" else "Show more")
             }
         }
@@ -71,6 +98,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     Week1Theme {
-        myApp(listOf("World", "Compose"))
+        Greetings()
     }
 }
