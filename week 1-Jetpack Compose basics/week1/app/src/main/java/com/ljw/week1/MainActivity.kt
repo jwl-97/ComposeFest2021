0d92c417b9a1e19353c7f5872ed7fe1d28e1827f
jwl-97 <jwlee_97@naver.com>
2021-11-07 13:13:24 +0900

    add composable function

--- week 1-Jetpack Compose basics/week1/app/src/main/java/com/ljw/week1/MainActivity.kt
+++ week 1-Jetpack Compose basics/week1/app/src/main/java/com/ljw/week1/MainActivity.kt
@@ -3,11 +3,15 @@ package com.ljw.week1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ljw.week1.ui.theme.Week1Theme
 
 class MainActivity : ComponentActivity() {
@@ -16,24 +20,28 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             Week1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                myApp()
             }
         }
     }
 }
 
 @Composable
+fun myApp() {
+    Surface(color = MaterialTheme.colors.primary) {
+        Greeting("Android")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Week1Theme {
-        Greeting("Android")
+        myApp()
     }
 }
