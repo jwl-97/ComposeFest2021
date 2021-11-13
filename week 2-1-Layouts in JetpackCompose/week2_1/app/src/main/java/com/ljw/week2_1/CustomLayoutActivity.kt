aed57d64294b218dac5d2852a61f00379a58b480
jwl-97 <jwlee_97@naver.com>
2021-11-13 22:46:42 +0900

    separate custom layout code from main

---
+++ week 2-1-Layouts in JetpackCompose/week2_1/app/src/main/java/com/ljw/week2_1/CustomLayoutActivity.kt
@@ -0,0 +1,62 @@
+package com.ljw.week2_1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ljw.week2_1.ui.theme.Week2_1Theme
+
+class CustomLayoutActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {}
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Layout(
+        modifier = modifier,
+        content = content
+    ) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints)
+        }
+
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun BodyContentColumn(modifier: Modifier = Modifier) {
+    MyOwnColumn(modifier.padding(8.dp)) {
+        Text("MyOwnColumn")
+        Text("places items")
+        Text("vertically.")
+        Text("We've done it by hand!")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CustomLayoutDefaultPreview() {
+    Week2_1Theme {
+        BodyContentColumn()
+    }
+}
--- week 2-1-Layouts in JetpackCompose/week2_1/app/src/main/java/com/ljw/week2_1/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/week2_1/app/src/main/java/com/ljw/week2_1/MainActivity.kt
@@ -18,7 +18,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -147,47 +146,12 @@ fun ImageList() {
     }
 }
 
-@Composable
-fun MyOwnColumn(
-    modifier: Modifier = Modifier,
-    content: @Composable () -> Unit
-) {
-    Layout(
-        modifier = modifier,
-        content = content
-    ) { measurables, constraints ->
-        val placeables = measurables.map { measurable ->
-            measurable.measure(constraints)
-        }
-
-        var yPosition = 0
-
-        layout(constraints.maxWidth, constraints.maxHeight) {
-            placeables.forEach { placeable ->
-                placeable.placeRelative(x = 0, y = yPosition)
-                yPosition += placeable.height
-            }
-        }
-    }
-}
-
-@Composable
-fun BodyContentColumn(modifier: Modifier = Modifier) {
-    MyOwnColumn(modifier.padding(8.dp)) {
-        Text("MyOwnColumn")
-        Text("places items")
-        Text("vertically.")
-        Text("We've done it by hand!")
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     Week2_1Theme {
 //        PhotographerCard()
 //        LayoutsCodelab()
-//        ImageList()
-        BodyContentColumn()
+        ImageList()
     }
 }
