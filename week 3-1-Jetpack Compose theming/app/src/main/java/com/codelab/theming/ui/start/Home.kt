a1236354d3b9041a292ffc3d1daba4860fe1d6c6
jwl-97 <jwlee_97@naver.com>
2021-11-20 23:34:44 +0900

    edit theme

--- week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ week 3-1-Jetpack Compose theming/app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -54,13 +54,15 @@ import androidx.compose.ui.unit.dp
 import com.codelab.theming.R
 import com.codelab.theming.data.Post
 import com.codelab.theming.data.PostRepo
+import com.codelab.theming.ui.start.theme.JetnewsTheme
 import java.util.Locale
 
 @Composable
 fun Home() {
     val featured = remember { PostRepo.getFeaturedPost() }
     val posts = remember { PostRepo.getPosts() }
-    MaterialTheme {
+
+    JetnewsTheme {
         Scaffold(
             topBar = { AppBar() }
         ) { innerPadding ->
@@ -204,20 +206,32 @@ fun PostItem(
     )
 }
 
-@Preview("Post Item")
-@Composable
-private fun PostItemPreview() {
-    val post = remember { PostRepo.getFeaturedPost() }
-    Surface {
-        PostItem(post = post)
-    }
-}
+//@Preview("Post Item")
+//@Composable
+//private fun PostItemPreview() {
+//    val post = remember { PostRepo.getFeaturedPost() }
+//    Surface {
+//        PostItem(post = post)
+//    }
+//}
 
 @Preview("Featured Post")
 @Composable
 private fun FeaturedPostPreview() {
     val post = remember { PostRepo.getFeaturedPost() }
-    FeaturedPost(post = post)
+
+    JetnewsTheme {
+        FeaturedPost(post = post)
+    }
+}
+
+@Preview("Featured Post • Dark")
+@Composable
+private fun FeaturedPostDarkPreview() {
+    val post = remember { PostRepo.getFeaturedPost() }
+    JetnewsTheme(darkTheme = true) {
+        FeaturedPost(post = post)
+    }
 }
 
 @Preview("Home")
